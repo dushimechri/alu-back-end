4685f5be10676479666d91b8c116d8eb4f6ff107
dushimechri <d.christoph@alustudent.com>
2025-02-14 19:57:52 +0200

    Update 2-export_to_JSON.py
    task done

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,35 +1,52 @@
 #!/usr/bin/python3
 """
-Request from API; Return TODO list progress given employee ID
-Export this data to JSON
+    python script that exports data in the JSON format
 """
-from sys import argv
 import json
 import requests
-
-
-def to_json():
-    """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for u in users.json():
-        if u.get('id') == int(argv[1]):
-            USERNAME = (u.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for t in todos.json():
-        if t.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
-
-    """export to json"""
-    t = []
-    for task in TASK_STATUS_TITLE:
-        t.append({"task": task[1], "completed": task[0], "username": USERNAME})
-    data = {str(argv[1]): t}
-    filename = "{}.json".format(argv[1])
-    with open(filename, "w") as f:
-        json.dump(data, f)
+from sys import argv
 
 
 if __name__ == "__main__":
-    to_json()
+    """
+        request user info by employee ID
+    """
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    """
+        convert json to dictionary
+    """
+    user = json.loads(request_employee.text)
+    """
+        extract username
+    """
+    username = user.get("username")
+
+    """
+        request user's TODO list
+    """
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    """
+        list to store task status(completed) in dictionary format
+    """
+    tasks = []
+    """
+        convert json to list of dictionaries
+    """
+    user_todos = json.loads(request_todos.text)
+    """
+        loop through dictionary & get completed tasks
+    """
+    for dictionary in user_todos:
+        task = {}
+        task["task"] = dictionary.get("title")
+        task["completed"] = dictionary.get("completed")
+        task["username"] = username
+        tasks.append(task)
+
+    """
+        export to JSON
+    """
+    with open('{}.json'.format(argv[1]), 'w') as json_file:
+        json.dump({argv[1]: tasks}, json_file)
